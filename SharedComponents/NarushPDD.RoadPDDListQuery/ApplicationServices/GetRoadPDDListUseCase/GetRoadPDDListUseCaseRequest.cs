5f181be3061f952dd505a3d0af2a6e4270986ec8
Maxim <51398110+M-D-G@users.noreply.github.com>
2020-05-21 15:50:51 +0300

    Add files via upload

---
+++ SharedComponents/NarushPDD.RoadPDDListQuery/ApplicationServices/GetRoadPDDListUseCase/GetRoadPDDListUseCaseRequest.cs
@@ -0,0 +1,30 @@
+﻿using NarushPDD.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NarushPDD.ApplicationServices.GetRoadPDDListUseCase
+{
+    public class GetRoadPDDListUseCaseRequest : IUseCaseRequest<GetRoadPDDListUseCaseResponse>
+    {
+        public string RecordedV { get; private set; }
+        public long? RoadPDDId { get; private set; }
+
+        private GetRoadPDDListUseCaseRequest()
+        { }
+
+        public static GetRoadPDDListUseCaseRequest CreateAllRoadPDDsRequest()
+        {
+            return new GetRoadPDDListUseCaseRequest();
+        }
+
+        public static GetRoadPDDListUseCaseRequest CreateRoadPDDRequest(long roadpddId)
+        {
+            return new GetRoadPDDListUseCaseRequest() { RoadPDDId = roadpddId };
+        }
+        public static GetRoadPDDListUseCaseRequest CreateRecordedVCriteriaRequest(string recordedv)
+        {
+            return new GetRoadPDDListUseCaseRequest() { RecordedV = recordedv };
+        }
+    }
+}
