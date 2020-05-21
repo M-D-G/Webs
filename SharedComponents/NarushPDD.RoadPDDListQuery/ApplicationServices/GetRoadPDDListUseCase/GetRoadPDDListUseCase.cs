282e2ebdbc6f8be77085a738f9b17c2e593de54b
Maxim <51398110+M-D-G@users.noreply.github.com>
2020-05-21 15:49:47 +0300

    Add files via upload

---
+++ SharedComponents/NarushPDD.RoadPDDListQuery/ApplicationServices/GetRoadPDDListUseCase/GetRoadPDDListUseCase.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using NarushPDD.DomainObjects;
+using NarushPDD.DomainObjects.Ports;
+using NarushPDD.ApplicationServices.Ports;
+
+namespace NarushPDD.ApplicationServices.GetRoadPDDListUseCase
+{
+    public class GetRoadPDDListUseCase : IGetRoadPDDListUseCase
+    {
+        private readonly IReadOnlyRoadPDDRepository _readOnlyRoadPDDRepository;
+
+        public GetRoadPDDListUseCase(IReadOnlyRoadPDDRepository readOnlyRoadPDDRepository) 
+            => _readOnlyRoadPDDRepository = readOnlyRoadPDDRepository;
+
+        public async Task<bool> Handle(GetRoadPDDListUseCaseRequest request, IOutputPort<GetRoadPDDListUseCaseResponse> outputPort)
+        {
+            IEnumerable<RoadPDD> roadpdds = null;
+            if (request.RoadPDDId != null)
+            {
+                var roadpdd = await _readOnlyRoadPDDRepository.GetRoadPDD(request.RoadPDDId.Value);
+                roadpdds = (roadpdd != null) ? new List<RoadPDD>() { roadpdd } : new List<RoadPDD>();
+                
+            }
+            else if (request.RecordedV != null)
+            {
+                roadpdds = await _readOnlyRoadPDDRepository.QueryRoadPDDs(new RecordedVCriteria(request.RecordedV));
+            }
+            else
+            {
+                roadpdds = await _readOnlyRoadPDDRepository.GetAllRoadPDDs();
+            }
+            outputPort.Handle(new GetRoadPDDListUseCaseResponse(roadpdds));
+            return true;
+        }
+    }
+}
